cf0c6fa3710e9632804318c3046bbfe65578d123
sondc <sondcse03564@fpt.edu.vn>
2017-06-06 11:11:54 +0700

    commit login (04)

--- Project/app/Http/Controllers/LoginController.php
+++ Project/app/Http/Controllers/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Auth\Access\Response;
 use Illuminate\Http\Request;
-use App\Http\DAO\AccountDAO;
+use App\Http\DAO\LoginDAO;
 
 define("ErrorLoginMesg", "Your username or password aren't correct. Please try again...");
 define("SuccessMegLogin", "Dang nhap thanh cong");
@@ -17,19 +17,16 @@ class LoginController extends Controller
 
     public function CheckAcc($userName,$password){
 
+        $loginDAO = new LoginDAO();
+        $resultCheck = $loginDAO->checkAcc($userName,$password) ;
 
-        //$userName = $request->userName;
-        //$password = $request->password;
-
-        $accDAO = new AccountDAO();
-        $resultCheck = $accDAO->checkAcc($userName,$password) ;
-
+        // If result = false then turn back to login form
         if ( !$resultCheck ){
             return back()->withInput()->with("LoginMeg",ErrorLoginMesg);
         } else{
 
             //get User Infomation
-            $userInfo =  $accDAO->getUserInfo($userName,$password);
+            $userInfo =  $loginDAO->getUserInfo($userName,$password);
             session(['USER_INFO' => $userInfo]);
 
             return view('Layout.Index');
