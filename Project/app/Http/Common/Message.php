4fba0c36c2dd3cc52e1184a97b034bb967eb0af0
QUYEN <quyentbse02724@fpt.edu.vn>
2017-07-19 11:41:24 +0700

    update message

--- Project/app/Http/Common/Message.php
+++ Project/app/Http/Common/Message.php
@@ -11,8 +11,20 @@ namespace App\Http\Common;
 
 class Message
 {
-    const MSG0001 = "MSG0001";
-    const MSG0002 = "MSG0002";
-    const MSG0003 = "MSG0003";
-    const MSG0004 = "MSG0004";
+    const MSG0001 = "Tên đang nhập không đúng";
+    const MSG0002 = "Tài khoản của bạn không tồn tại";
+    const MSG0003 = "Hãy nhập mật khẩu";
+    const MSG0004 = "Mật khẩu sai, tài khoản sẽ bị khóa nếu đăng nhập sai 3 lần";
+    const MSG0005 = "Các mục bắt buộc không được trống";
+    const MSG0006 = "Hãy chọn phương thức thanh toán";
+    const MSG0007 = "Hãy chọn câu trả lời";
+    const MSG0008 = "Chọn xác nhận thông tin trước khi xử lý";
+    const MSG0009 = "Thông tin nhập vào không đúng";
+    const MSG0010 = "Tên loại phòng không được để trống";
+    const MSG0011 = "Loại phòng này đã tồn tại";
+    const MSG0012 = "Tên Thiết bị không được để trống";
+    const MSG0013 = "Hãy nhập dữ liệu";
+    const MSG0014 = "Dữ liệu nhập vào không đúng";
+
+
 }
