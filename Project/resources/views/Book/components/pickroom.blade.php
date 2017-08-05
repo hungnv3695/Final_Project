99a63c0e563e0012e870c5551303faff55031b3f
manhdd <manhddse03539@gmail.com>
2017-08-05 12:36:22 +0700

    book update

--- Project/resources/views/Book/components/pickroom.blade.php
+++ Project/resources/views/Book/components/pickroom.blade.php
@@ -1,32 +1,97 @@
+<style type="text/css">
+    .label1{
+        width:120px;
+        text-align:left;
+		font-size:120%;
+		text-decoration:underline;
+    }
+	.label2{
+        line-height:40px;
+    }
+</style>
 <div class="pickroom">
     <div class="container">
         <div class="row">
             <div class="col-md-3">
                 <div class="form-group">
-                    <label for="checkin"><b>Check in date</b></label>
+                    <label for="checkin">Check in date</label>
                     <input type="text" class="form-control" id="checkin" placeholder="Check in date">
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="form-group">
-                    <label for="checkout"><b>Check out date</b></label>
+                    <label for="checkout">Check out date</label>
                     <input type="text" class="form-control" id="checkout" placeholder="Check out date">
                 </div>
             </div>
             <div class="col-md-2">
                 <div class="form-group">
-                    <label for="adult"><b>Adult</b></label>
-                    <input type="number" class="form-control" id="adult" min="0">
+                    <label for="adult">Adult</label>
+                    <input type="number" class="form-control" id="adult" min="1">
                 </div>
             </div>
             <div class="col-md-2">
                 <div class="form-group">
-                    <label for="child"><b>Children</b></label>
+                    <label for="child">Children</label>
                     <input type="number" class="form-control" id="child" min="0">
                 </div>
             </div>
-            <div class="col-md-2 justify-content-center" style="margin-top:32px;">
+            <div class="col-md-2 justify-content-center" style="margin-top:26px;">
                 <button class="btn btn-primary">Apply</button>
+            </div>
+        </div>
+		<div class="row">
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label class="label1" for="roomtype"><b>Kiểu phòng</b></label>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+					<label class="label1" for="roomleft"><b>Phòng trống</b></label>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="form-group">
+                    <label class="label1" for="price"><b>Giá</b></label>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="form-group">
+                    <label class="label1" for="quantity"><b>Số lượng</b></label>
+                </div>
+            </div>
+			<div class="col-md-2">
+                <div class="form-group">
+                    <label class="label1" for="quantity"><b>Tổng tiền</b></label>
+                </div>
+            </div>
+        </div>
+		<div class="row">
+            <div class="col-md-3">
+                <div class="form-group">
+                    <span class="fa fa-home" style="margin-right:5px;"></span><label class="label2"><b>Single</b></label>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+					 <label class="label2"><b>15</b></label>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="form-group">
+                    <label class="label2"><b>$190</b></label>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="form-group">
+                    <input type="number" class="form-control" id="quantity" min="1">
+                </div>
+            </div>
+			<div class="col-md-2">
+                <div class="form-group">
+                    <label class="label2"><b>$390</b></label>
+                </div>
             </div>
         </div>
     </div>
