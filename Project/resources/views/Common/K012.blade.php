1defd7d0fed52fb0503378aacd92702adf6880f1
manhdd <manhddse03539@gmail.com>
2017-08-10 17:36:41 +0700

    fix k012

--- Project/resources/views/Common/K012.blade.php
+++ Project/resources/views/Common/K012.blade.php
@@ -59,26 +59,26 @@
                     <div class="col-md-12" style="margin-top:20px;margin-bottom:20px;border: 1px solid #898989;border-radius:10px;">
                         <div class="form-inline col-md-offset-1" style="margin-top:20px;">
                             <label class="label1" for="">Tên tài khoản: </label>
-                            <input id="txtAccountName" name="txtAccountName" type="text" class="form-control input-md" size="20" value="{!! $user[0]->user_id !!}"readonly >
+                            <input id="txtAccountName" name="txtAccountName" type="text" class="form-control input-md" size="27" value="{!! $user[0]->user_id !!}"readonly >
                         </div>
                         <div class="form-inline col-md-offset-1" style="margin-top:20px;">
                             <label class="label1" for="">Vị trí: </label>
                             @if($user[0]->group_cd == 'G01')
-                                <input id="txtPosition" name="txtPosition" type="text" class="form-control input-md" size="20" value="Manager" readonly>
+                                <input id="txtPosition" name="txtPosition" type="text" class="form-control input-md" size="27" value="Manager" readonly>
                             @elseif($user[0]->group_cd == 'G02')
-                                <input id="txtPosition" name="txtPosition" type="text" class="form-control input-md" size="20" value="Receptionist" readonly>
+                                <input id="txtPosition" name="txtPosition" type="text" class="form-control input-md" size="27" value="Receptionist" readonly>
                             @elseif($user[0]->group_cd == 'G03')
-                                <input id="txtPosition" name="txtPosition" type="text" class="form-control input-md" size="20" value="Accountant" readonly>
+                                <input id="txtPosition" name="txtPosition" type="text" class="form-control input-md" size="27" value="Accountant" readonly>
                             @endif
                         </div>
                         <div class="form-inline col-md-offset-1" style="margin-top:20px;">
                             <label class="label1" for="">Trạng thái: </label>
                             @if($user[0]->delete_flg == '1')
-                                <input id="txtStatus" name="txtStatus" type="text" class="form-control input-md" size="20" value="Không hoạt động" readonly >
+                                <input id="txtStatus" name="txtStatus" type="text" class="form-control input-md" size="27" value="Không hoạt động" readonly >
                             @elseif ($user[0]->acc_lock_flg == '1')
-                                <input id="txtStatus" name="txtStatus" type="text" class="form-control input-md" size="20" value="Đang bị khóa" readonly>
+                                <input id="txtStatus" name="txtStatus" type="text" class="form-control input-md" size="27" value="Đang bị khóa" readonly>
                             @else
-                                <input id="txtStatus" name="txtStatus" type="text" class="form-control input-md" size="20" value="Đang hoạt động" readonly>
+                                <input id="txtStatus" name="txtStatus" type="text" class="form-control input-md" size="27" value="Đang hoạt động" readonly>
                             @endif
 
                         </div>
@@ -87,36 +87,36 @@
                             <?php
                             $date = $user[0]->create_ymd;
                             ?>
-                            <input id="txtCreateDay" name="txtCreateDay" type="text" class="form-control input-md" size="20" value="{!! $date !!} " readonly>
+                            <input id="txtCreateDay" name="txtCreateDay" type="text" class="form-control input-md" size="27" value="{!! $date !!} " readonly>
                         </div>
-                        <div class="col-md-6 col-md-offset-3" style="margin-top:20px;margin-bottom:20px;"><button class="btn btn-primary btn-block" type="button" value="btnchangepass" name="btnchangepass" onclick="window.location='{{ url("/K012/K012_1") }}'" ><b>Đổi mật khẩu</b></button></div>
+                        <div class="col-md-7 col-md-offset-3" style="margin-top:20px;margin-bottom:20px;"><button class="btn btn-primary btn-block" type="button" value="btnchangepass" name="btnchangepass" onclick="window.location='{{ url("/K012/K012_1") }}'" ><b>Đổi mật khẩu</b></button></div>
                     </div>
                 </div>
                 <div id="panel2">
                     <div class="col-md-12" style="margin-top:20px;margin-bottom:20px;border: 1px solid #898989;border-radius:10px;">
                         <div class="form-inline col-md-offset-1" style="margin-top:20px;">
                             <label class="label1" for="">Họ tên: </label>
-                            <input id="txtFullName" name="txtFullName" type="text" class="form-control input-md" maxlength="50" size="20" value="{!! $user[0]->user_name !!}" oninvalid="InvalidMsg(this);" required  >
+                            <input id="txtFullName" name="txtFullName" type="text" class="form-control input-md" maxlength="50" size="25" value="{!! $user[0]->user_name !!}" oninvalid="InvalidMsg(this);" required  >
                         </div>
                         <div class="form-inline col-md-offset-1" style="margin-top:20px;">
                             <label class="label1" for="">Địa chỉ: </label>
-                            <input id="txtAddress" name="txtAddress" type="text" class="form-control input-md" size="20" maxlength="100"  value="{!! $user[0]->address !!}" oninvalid="InvalidMsg(this);" required  >
+                            <input id="txtAddress" name="txtAddress" type="text" class="form-control input-md" size="25" maxlength="100"  value="{!! $user[0]->address !!}" oninvalid="InvalidMsg(this);" required  >
                         </div>
                         <div class="form-inline col-md-offset-1" style="margin-top:20px;">
                             <label class="label1" for="">CMND: </label>
-                            <input id="txtIdCard" name="txtIdCard" type="text" class="form-control input-md" size="20" maxlength="12"  value="{!! $user[0]->identity_card !!}" oninvalid="InvalidMsg(this);" required >
+                            <input id="txtIdCard" name="txtIdCard" type="text" class="form-control input-md" size="25" maxlength="12"  value="{!! $user[0]->identity_card !!}" oninvalid="InvalidMsg(this);" required >
                         </div>
                         <div class="form-inline col-md-offset-1" style="margin-top:20px;">
                             <label class="label1" for="">Nơi cấp: </label>
-                            <input id="txtLocation" name="txtLocation" type="text" class="form-control input-md" size="20"  maxlength="20" value="{!! $user[0]->tax_code !!}"  oninvalid="InvalidMsg(this);" required >
+                            <input id="txtLocation" name="txtLocation" type="text" class="form-control input-md" size="25"  maxlength="20" value="{!! $user[0]->tax_code !!}"  oninvalid="InvalidMsg(this);" required >
                         </div>
 						<div class="form-inline col-md-offset-1" style="margin-top:20px;">
                             <label class="label1" for="">Điện thoại: </label>
-                            <input id="txtPhone" name="txtPhone" type="text" class="form-control input-md" size="20" maxlength="20"  value="{!! $user[0]->phone !!}" oninvalid="InvalidMsg(this);" required  >
+                            <input id="txtPhone" name="txtPhone" type="text" class="form-control input-md" size="25" maxlength="20"  value="{!! $user[0]->phone !!}" oninvalid="InvalidMsg(this);" required  >
                         </div>
                         <div class="form-inline col-md-offset-1" style="margin-top:20px;margin-bottom:20px;">
                             <label class="label1" for="">Email: </label>
-                            <input id="txtEmail" name="txtEmail" type="email" class="form-control input-md" size="20" maxlength="50"  value="{!! $user[0]->mail !!}" oninvalid="InvalidMsg(this);" oninput="InvalidMsg(this);" required  >
+                            <input id="txtEmail" name="txtEmail" type="email" class="form-control input-md" size="25" maxlength="50"  value="{!! $user[0]->mail !!}" oninvalid="InvalidMsg(this);" oninput="InvalidMsg(this);" required  >
                         </div>
                     </div>
                 </div>
@@ -124,13 +124,13 @@
             <div class="col-md-6 col-md-offset-3" style="background-color:#c3bfc0;margin-bottom:10px;">
                 <div id="panel3">
                     <div class="form-inline col-md-offset-10" style="margin-top:10px;margin-bottom:10px;">
-                        <button type="button" class="btn btn-danger col-md-offset-5" value="bntBack" name="bntBack" onclick="window.location='{{ url("/K002") }}'" ><b>Back</b></button>
+                        <button type="button" class="btn btn-danger col-md-offset-2" value="bntBack" name="bntBack" onclick="window.location='{{ url("/K002") }}'" ><b>Quay lại</b></button>
                     </div>
                 </div>
                 <div id="panel4">
                     <div class="form-inline col-md-offset-9" style="margin-top:10px;margin-bottom:10px;">
-                        <button class="btn btn-success col-md-offset-2" value="bntSave" name="bntSave"><b>Lưu</b></button>
-                        <button type="button" class="btn btn-danger" value="bntBack" name="bntBack"  onclick="window.location='{{ url("/K002") }}'" ><b>Back</b></button>
+                        <button class="btn btn-success" value="bntSave" name="bntSave"><b>Lưu</b></button>
+                        <button type="button" class="btn btn-danger" value="bntBack" name="bntBack"  onclick="window.location='{{ url("/K002") }}'" ><b>Quay lại</b></button>
                     </div>
                 </div>
             </div>
